6c4784ec2bb362f33843e4f7dc10e2667e928d44
Mukesh Kumar <mrajnitro@gmail.com>
2020-01-24 11:19:47 +0530

    initial commit

---
+++ employee-service/src/main/java/com/jwt/ms/demo/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.jwt.ms.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class EmployeeController {
+
+	@Value("${app.service-name}")
+	private String serviceName;
+
+	@Value("${app.shared.attribute}")
+	private String sharedAttribute;
+
+	@GetMapping("/service")
+	public String getServiceName() {
+		return "service name [" + this.serviceName + "]";
+	}
+
+	@GetMapping("/shared")
+	public String getSharedAttribute() {
+		return " application.properties [" + this.sharedAttribute + "]";
+	}
+
+}
